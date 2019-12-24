9c77aca897be3059fcd9b450559ea2ff8a541c8e
Tymur Mikava <36811813+timKava@users.noreply.github.com>
2019-12-25 00:31:20 +0300

    Update task2.js

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -4,11 +4,11 @@ a = parseInt(prompt('Enter first side of triangle: ', ''));
 b = parseInt(prompt('Enter first side of triangle: ', ''));
 c = parseInt(prompt('Enter first side of triangle: ', ''));
 
-if (a === '' || a === undefined || b === '' || b === undefined || c === '' || c === undefined){
+if (isNaN(a) || isNaN(b) || isNaN(c)){
     alert('input values should be ONLY numbers ');
 } else if (a === 0 || b === 0 || c === 0){
     alert('A triangle must have 3 sides with a positive definite length');
-} else if (a + b < c || a + c < b || b + c < a){
+} else if (a + b <= c || a + c <= b || b + c <= a){
     alert('Triangle doesn’t exist');
 } else if (a === b && a === c){
     console.log('Equilateral triangle');
